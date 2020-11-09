5724d37fef254c8eb446c776146c47f0aaed4f52
christopherayork <christopherayork1992@gmail.com>
2020-11-09 16:49:26 -0500

    building route for creatures, pulled in error wrapper

---
+++ Backend/routes/creatures.js
@@ -0,0 +1,30 @@
+const Router = require("express").Router();
+const Creature = require("../database/controllers/creature");
+const errorWrapper = require("../utility/errorWrapper");
+
+
+
+Router.route("/")
+    .get(errorWrapper(async (req, res) => {
+        return Creature.readAll(req, res);
+    }))
+    .post(errorWrapper(async (req, res) => {
+        // auth required
+        return Creature.create(req, res);
+    }));
+
+Router.route("/:id")
+    .get(errorWrapper(async (req, res) => {
+        if(!req.params.id) return res.status(400).json({ error: "id param required" });
+        else return Creature.read(req, res);
+    }))
+    .put(errorWrapper(async (req, res) => {
+        // auth required
+        if(!req.params.id) return res.status(400).json({ error: "id param required" });
+        else return Creature.update(req, res);
+    }))
+    .delete(errorWrapper(async (req, res) => {
+        // auth required
+        if(!req.params.id) return res.status(400).json({ error: "id param required" });
+        else return Creature.delete(req, res);
+    }));s
