29209a09893910471f1bf899abe811d44db9e56c
christopherayork <christopherayork1992@gmail.com>
2020-11-12 19:27:07 -0500

    Building out more of the form functionality and formatting. Some abstraction will be required to extend to other form crud ops

--- Frontend/app/src/Components/Forms/Creature/Create.js
+++ Frontend/app/src/Components/Forms/Creature/Create.js
@@ -1,37 +1,76 @@
 import React, { useState } from "react";
-import { TextField } from "@material-ui/core";
+import { Input } from "@material-ui/core";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
+let hardTemplate = {
+    stat1: { value: 2, max: 5 },
+    stat2: { value: 4, max: 8 },
+    stat3: { value: 3, max: 12 },
+    custom: {
+        custom1: { value: 6, max: 11 },
+        custom2: { value: 1, max: 4 }
+    }
+};
 
 const Create = (props) => {
-    const [fields, setFields] = useState({});
+    const [fields, setFields] = useState(hardTemplate);
     // retrieve template for default fields from backend
     // lets mock it for now
-    const updateField = (event) => {
-        setFields({ ...fields, [event.target.name]: event.target.value });
+    const updateField = (event, tag) => {
+        setFields({
+            ...fields,
+            [event.target.name]: {
+                ...fields[event.target.name],
+                [tag]: event.target.value
+            }
+        });
     };
 
     // it has to nest properly to work out correctly
-    const updateFieldCustom = (event) => {
-        setFields({ ...fields, custom: { ...fields.custom, [event.target.name]: event.target.value } });
+    const updateFieldCustom = (event, tag) => {
+        setFields({
+            ...fields,
+            custom: {
+                ...fields.custom,
+                // each field holds a stat of the format -> stat: { value: int, max: int }
+                // we want to keep the other and change the target using the tag, which will either be 'value' or 'max'
+                [event.target.name]: {
+                    ...fields[event.target.name],
+                    [tag]: event.target.value
+                }
+            }
+        });
     };
-
-    let hardTemplate = {
-        stat1: { value: 2, max: 5 },
-        stat2: { value: 4, max: 8 },
-        stat3: { value: 3, max: 12 },
-        custom: {
-            custom1: { value: 6, max: 11 },
-            custom2: { value: 1, max: 4 }
-        }
-    };
+    let keys = Object.keys(fields);
+    let customKeys = Object.keys(fields["custom"]);
+    let defaults = keys.filter(v => v !== "custom");
+    let vals = defaults.map(v => {
+        return <Stat key={v} name={v} setter={updateField} stat={fields[v]} />
+    });
+    let customVals = customKeys.map(v => {
+        return <Stat key={v} name={v} setter={updateFieldCustom} stat={fields["custom"][v]} />
+    });
     return (
         <form>
-            {Object.keys(hardTemplate).map(v => {
-                return v !== "custom" ?
-                    <Stat name={v} setter={updateField} stat={hardTemplate[v]} /> :
-                    Object.keys(hardTemplate[v]).map(vi => {
-                        return <Stat name={v} setter={updateFieldCustom} stat={hardTemplate[v][vi]} />
-                    });
-            })}
+            <Table className='stat table'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Stat</TableCell>
+                        <TableCell>Value</TableCell>
+                        <TableCell>Max</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {vals}
+                    {customVals}
+                </TableBody>
+            </Table>
         </form>
     );
 };
@@ -42,11 +81,29 @@ const Stat = (props) => {
     // we need to sort out the name part of the fields
     // default stats will show the name but have the input locked
     // we need props to match { name, stat: { value, max }, setter() }
+    let applyProps = (tag) => {
+        return {
+            key: `${props.name}-${tag}`,
+            id: `${props.name}-${tag}`,
+            name: props.name,
+            label: `${props.name} ${tag}`,
+            value: props.stat[tag], // its very important that tag always matches the attribute key
+            variant: 'outlined',
+            onChange: (event) => props.setter(event, tag) // tell it whether its value or max
+        };
+    }
     return (
-        <div className='stat' id={props.name}>
-            <TextField id={props.name + '-value'} name={props.name + '-max'} label='standard' value={props.stat.value} onChange={props.setter} />
-            <TextField id={props.name + '-max'} name={props.name + '-max'} label='standard' value={props.stat.max} onChange={props.setter} />
-        </div>
+        <TableRow className='stat' id={props.name}>
+            <TableCell>
+                {props.name}
+            </TableCell>
+            <TableCell>
+                <Input {...applyProps('value')} />
+            </TableCell>
+            <TableCell>
+                <Input {...applyProps('max')} />
+            </TableCell>
+        </TableRow>
     );
 };
 
